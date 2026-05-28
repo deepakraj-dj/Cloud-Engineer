ac1c72fd5f260b32968bcc6c04c76230b1b99aa7
deepakraj-dj <deepakrajdj23@gmail.com>
2026-05-28 21:37:32 +0530

    file

---
+++ javv.js
@@ -0,0 +1,6 @@
+
+     document.getElementById("loginForm").addEventListener("submit", async function(e) {
+      e.preventDefault(); // prevent page reload
+
+     let d = document.getElementById("loginForm")
+     console.log(d)
