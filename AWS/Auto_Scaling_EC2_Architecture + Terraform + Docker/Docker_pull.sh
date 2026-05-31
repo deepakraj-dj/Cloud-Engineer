6dfbd8a5c2fee85a124c222eca5708a63f878c34
Deepakraj K <deepakrajdj23@gmail.com>
2026-05-31 20:51:28 +0530

    Update Docker_pull.sh

--- AWS/Auto_Scaling_EC2_Architecture + Terraform + Docker/Docker_pull.sh
+++ AWS/Auto_Scaling_EC2_Architecture + Terraform + Docker/Docker_pull.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 ECR_REGISTRY=333372137025.dkr.ecr.us-east-1.amazonaws.com/portfolio/test:latest
+AWS_CLI_Link="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" 
 sudo apt update -y
-sudo apt install awscli -y
+sudo apt install unzip -y
 sudo apt install docker.io -y 
+curl "${AWS_CLI_Link}" -o awscliv2.zip
+unzip awscliv2.zip
+./aws/install
 sudo systemctl start docker 
 while ! sudo systemctl is-active --quiet docker; do
     sleep 1
