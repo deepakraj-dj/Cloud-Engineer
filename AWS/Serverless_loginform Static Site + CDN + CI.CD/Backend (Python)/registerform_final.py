e4d5f8963dce2526417d2a1d6178fbcfb18898f4
Deepakraj K <deepakrajdj23@gmail.com>
2026-06-03 15:22:41 +0530

    Update registerform_final.py

--- AWS/Serverless_loginform Static Site + CDN + CI.CD/Backend (Python)/registerform_final.py
+++ AWS/Serverless_loginform Static Site + CDN + CI.CD/Backend (Python)/registerform_final.py
@@ -35,7 +35,7 @@ def lambda_handler(event,context):
             "DOB":dob
             }
             )
-        # REMOVED the intermediate return from here so execution continues downw
+        # REMOVED the intermediate return from here so execution continues downward
         
     except Exception as d:
         print(f"Something Went Wrong,{d} Could not be inserted")
